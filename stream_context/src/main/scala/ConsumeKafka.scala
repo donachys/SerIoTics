d70ef9eb5e8645745d0d25365ce4c8bf14ee97e7
SD <donachys@gmail.com>
2015-09-17 16:17:01 -0700

    remove the kafka.serializer.StringDecoder import

--- stream_context/src/main/scala/ConsumeKafka.scala
+++ stream_context/src/main/scala/ConsumeKafka.scala
@@ -2,7 +2,7 @@
 import org.apache.spark.streaming._
 import com.datastax.spark.connector.streaming._
 
-import kafka.serializer.StringDecoder
+//import kafka.serializer.StringDecoder
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
