1a929c16b527bae590fa61b0bc7fcc76e53fcced
SD <donachys@gmail.com>
2015-09-17 14:24:09 -0700

    update package name in pom.xml, cleanup and formatting of KafkaProducer.java

--- data_generation/src/main/java/com/VeryLargeEntityMonitor/data_generation/DataGenKafkaProducer.java
+++ data_generation/src/main/java/com/VeryLargeEntityMonitor/data_generation/DataGenKafkaProducer.java
@@ -1,7 +1,4 @@
 package com.VeryLargeEntityMonitor.data_generation;
-// import kafka.javaapi.producer.Producer;
-// import kafka.producer.KeyedMessage;
-// import kafka.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
@@ -11,36 +8,26 @@ import java.util.Date;
 
 public class DataGenKafkaProducer{
 
-	public static void main(String... args){
-		long events = Long.parseLong(args[0]);
+    public static void main(String... args){
+        long events = Long.parseLong(args[0]);
         Random rnd = new Random();
- 
+
         Properties props = new Properties();
-        //props.put("metadata.broker.list", "ec2-54-153-2-109.us-west-1.compute.amazonaws.com:9092");
         props.put("bootstrap.servers", "ec2-54-153-2-109.us-west-1.compute.amazonaws.com:9092,ec2-54-153-26-178.us-west-1.compute.amazonaws.com:9092,ec2-54-153-48-210.us-west-1.compute.amazonaws.com:9092");
-        //props.put("serializer.class", "kafka.serializer.StringEncoder");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        //props.put("partitioner.class", "example.producer.SimplePartitioner");
-        //props.put("serializer.class","kafka.serializer.DefaultEncoder");
-        //props.put("producer.type", 1);
-        //props.put("request.required.acks", "1");
         props.put("acks", "1");
 
- 
-        //ProducerConfig config = new ProducerConfig(props);
- 
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);//config
- 
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
         for (long nEvents = 0; nEvents < events; nEvents++) { 
             System.out.println("nEvents: " + nEvents);
-               long runtime = new Date().getTime();  
-               String ip = "192.168.2." + rnd.nextInt(255); 
-               String msg = runtime + ",www.example.com," + ip; 
-               ProducerRecord<String, String> data = new ProducerRecord<String, String>("my-topic", ip, msg);
-               producer.send(data);
+            long runtime = new Date().getTime();  
+            String ip = "192.168.2." + rnd.nextInt(255); 
+            String msg = runtime + ",www.example.com," + ip; 
+            ProducerRecord<String, String> data = new ProducerRecord<String, String>("my-topic", ip, msg);
+            producer.send(data);
         }
         producer.close();
-	}
-
+    }
 }
