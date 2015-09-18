448da9abf9514aaa0be996a639763a59a7e75ad9
SD <donachys@gmail.com>
2015-09-17 20:02:47 -0700

    temporarily remove writing to cassandra

--- stream_context/src/main/scala/ConsumeKafka.scala
+++ stream_context/src/main/scala/ConsumeKafka.scala
@@ -38,10 +38,10 @@ object ConsumeKafka {
                                .agg("quantity" -> "avg", "quantity" -> "sum")
                                .orderBy("source_id")
         ticks_per_source_DF.show()
-        ticks_per_source_DF.write
-            .format("org.apache.spark.sql.cassandra")
-            .options(Map( "table" -> "mytopic2", "keyspace" -> "playground"))
-            .save()
+        // ticks_per_source_DF.write
+        //     .format("org.apache.spark.sql.cassandra")
+        //     .options(Map( "table" -> "mytopic2", "keyspace" -> "playground"))
+        //     .save()
     }
 
     // Start the computation
