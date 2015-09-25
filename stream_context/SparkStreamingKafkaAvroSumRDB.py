9cec8bb29753f7e95ec278eaf58d41c53bd0f527
SD <donachys@gmail.com>
2015-09-24 17:30:35 -0700

    save to db after computation

--- stream_context/SparkStreamingKafkaAvroSumRDB.py
+++ stream_context/SparkStreamingKafkaAvroSumRDB.py
@@ -7,6 +7,10 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
 import rethinkdb as r
 import json
 import os
@@ -37,6 +41,13 @@ if __name__ == "__main__":
 
 
     #kvs = KafkaUtils.createStream(ssc, zkQuorum, "my-topic2-consumer", {topic: 1})
+    def sendRDDCount(count):
+        #print('index: ' + str(index))
+        connection = createNewConnection()#todo: use-connection-pool
+        #print('count' + str(count))
+        #r.table(RDB_TABLE).filter(r.row["partition"] == index).update({"count": count}).run(connection)
+        r.table(RDB_TABLE).insert({"count": count, "time":time.time()}).run(connection)
+        connection.close()
     def sendPartitionCount(index, count):
         #print('index: ' + str(index))
         connection = createNewConnection()#todo: use-connection-pool
@@ -60,16 +71,19 @@ if __name__ == "__main__":
             print("-------##########################----------")
             print(rdd.getNumPartitions())
             print("-------##########################----------")
-    kafkaStreams[0].foreachRDD(lambda rdd: sendPartitionCount(0,rdd.count()))
-    kafkaStreams[1].foreachRDD(lambda rdd: sendPartitionCount(1,rdd.count()))
-    kafkaStreams[2].foreachRDD(lambda rdd: sendPartitionCount(2,rdd.count()))
-    kafkaStreams[3].foreachRDD(lambda rdd: sendPartitionCount(3,rdd.count()))
+    # kafkaStreams[0].foreachRDD(lambda rdd: sendPartitionCount(0,rdd.count()))
+    # kafkaStreams[1].foreachRDD(lambda rdd: sendPartitionCount(1,rdd.count()))
+    # kafkaStreams[2].foreachRDD(lambda rdd: sendPartitionCount(2,rdd.count()))
+    # kafkaStreams[3].foreachRDD(lambda rdd: sendPartitionCount(3,rdd.count()))
     #kkvvss.foreachRDD(lambda rdd: sendPartitionCount(0,rdd.count()))
     for idx,kvs in enumerate(kafkaStreams):
         #kvs.foreachRDD(printParts)
         records = kvs.map(lambda x: json.loads(x[1]))
         sums = records.map(lambda obj: (obj['unique_id'], obj['quantity'])) \
             .reduceByKey(lambda a, b: a+b)
+        kvs.foreachRDD(lambda rdd: sendRDDCount(rdd.count()));
+
+
         #sums.pprint(num=300)
         #kvs.count().pprint()
     #kkvvss.foreachRDD(printParts)
