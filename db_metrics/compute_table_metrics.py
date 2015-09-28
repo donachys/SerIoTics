66396deab0b9fa42c271a6828d0cd8f8792075c8
SD <donachys@gmail.com>
2015-09-27 20:19:05 -0700

    define function before calling it

--- db_metrics/compute_table_metrics.py
+++ db_metrics/compute_table_metrics.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     RDB_PORT = os.environ.get('RDB_PORT')
     RDB_DB = "SerIoTics"
     RDB_TABLE = sys.argv[1:]
-    connection = createNewConnection()
     def createNewConnection():
         return r.connect(host=RDB_HOST, port=RDB_PORT, db=RDB_DB)
+    connection = createNewConnection()
     def getStartTime():
         return r.table(RDB_TABLE).filter(r.row['count'].gt(0)).min('time').run(connection)
     def getStopTime():
