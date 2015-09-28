cca4526b448b6acfd0b9a99ddafecc60770b3beb
SD <donachys@gmail.com>
2015-09-28 12:46:55 -0700

    extend base.html once more

--- data_display/app/static/js/graph.js
+++ data_display/app/static/js/graph.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-	console.log('hi from grapgh.js!!')
-	console.log(chart_id)
-	$(chart_id).highcharts({
+	$('#chart_id').highcharts({
 		chart: chart,
 		title: title,
 		xAxis: xAxis,
