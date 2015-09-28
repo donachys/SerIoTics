2a580ea723676a59437f1d5a863bdd40264bf583
SD <donachys@gmail.com>
2015-09-27 17:01:09 -0700

    JSON_consumer spark streaming project

---
+++ JSON_consumer/SparkStreamingKafkaJSONSumRDB.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+
+import sys
+import time
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+import rethinkdb as r
+import json
+import os
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: SparkStreamingKafkaJSONSumRDB.py <zk> <topic> <window size (sec)> <tablename>", file=sys.stderr)
+        exit(-1)
+
+    RDB_HOST =  os.environ.get('RDB_HOST')
+    RDB_PORT = os.environ.get('RDB_PORT')
+    RDB_DB = "SerIoTics"
+    zkQuorum, topic, stream_window, RDB_TABLE = sys.argv[1:]
+    stream_window = int(stream_window)
+    
+    sc = SparkContext(appName="PythonStreamingKafkaJSONSums")
+    ssc = StreamingContext(sc, batchDuration=stream_window)
+
+    
+    streams = []
+    
+    numStreams = 4
+    kafkaStreams = [KafkaUtils.createStream(ssc, zkQuorum, "JSON-consumer", {topic: 1}) for _ in range (numStreams)]
+
+    def sendRDDCount(count):
+        connection = createNewConnection()
+        r.table(RDB_TABLE).insert({"count": count, "time":time.time()}).run(connection)
+    def sendPartitionCount(index, count):
+        connection = createNewConnection()
+        r.table(RDB_TABLE).insert({"partition":index, "count": count, "time":time.time()}).run(connection)
+        connection.close()
+    def sendPartition(iter):
+        connection = createNewConnection()
+        for record in iter:
+            r.table(RDB_TABLE).insert(json.loads(record[1])).run(connection)
+        connection.close()
+    def createNewConnection():
+        return r.connect(host=RDB_HOST, port=RDB_PORT, db=RDB_DB)
+    for idx,kvs in enumerate(kafkaStreams):
+        records = kvs.map(lambda x: json.loads(x[1]))
+        sums = records.map(lambda obj: (obj['unique_id'], obj['quantity'])) \
+            .reduceByKey(lambda a, b: a+b)
+        kvs.foreachRDD(lambda rdd: sendRDDCount(rdd.count()));
+
+    ssc.start()
+    ssc.awaitTermination()
