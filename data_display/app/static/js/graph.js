1a0531719eca6e98c47c860661b9d6f3eb5ab493
SD <donachys@gmail.com>
2015-09-29 10:29:44 -0700

    resume 1000 ms ajax request

--- data_display/app/static/js/graph.js
+++ data_display/app/static/js/graph.js
@@ -10,13 +10,15 @@ $(function () {
         chart : {
             events : {
                 load : function () {
+
                     // set up the updating of the chart each second
                     var series = this.series[0];
-                    var url = 'http://ec2-52-26-108-249.us-west-2.compute.amazonaws.com:5000/api/json_throughput';
-                    setInterval(function () {
+					setInterval(function () {
                         var x = (new Date()).getTime(), // current time
-                            y = $.getJSON(url, function(data){series.addPoint([x, data.result.records_per_second], true, true));
-                    });
+                            y = $.getJSON(url, function(data){
+                            	series.addPoint([x, data.result.records_per_second], true, true);
+                    		});
+                    }, 1000);
                 }
             }
         },
