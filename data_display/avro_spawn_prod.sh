7a29b0bdfe705f78473b64556e2b4105fc3be0d8
SD <donachys@gmail.com>
2015-10-01 19:37:39 -0700

    more producers for experiments

--- data_display/avro_spawn_prod.sh
+++ data_display/avro_spawn_prod.sh
@@ -1,4 +1,4 @@
 PRODSEED=$1
 tmux new-session -s p1 -n bash -d
 tmux new-window -t 1
-tmux send-keys -t p1:1 'bash ~/SerIoTics/Avro_producer/spawn_kafka_streams.sh 1 p1 '"$PRODSEED"'' C-m
+tmux send-keys -t p1:1 'bash ~/SerIoTics/Avro_producer/spawn_kafka_streams.sh 3 p1 '"$PRODSEED"'' C-m
--- data_display/json_start_prod.sh
+++ data_display/json_start_prod.sh
@@ -1,2 +1,3 @@
 ssh prod1 'bash -s' < json_spawn_prod.sh 100000
 ssh prod2 'bash -s' < json_spawn_prod.sh 200000
+ssh prod3 'bash -s' < json_spawn_prod.sh 200000
