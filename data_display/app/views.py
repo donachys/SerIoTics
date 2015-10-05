7810a5d699f6fbf02c5dfe4088157f4d157267bb
SD <donachys@gmail.com>
2015-10-05 10:42:39 -0700

    syntax correction in views.py

--- data_display/app/views.py
+++ data_display/app/views.py
@@ -50,7 +50,7 @@ def blog():
     title = "SerIoTics"
     return render_template("blog.html", title = title)
 @app.route('/slides_pdf')
-def slides_pdf:
+def slides_pdf():
     return app.send_static_file('Shaun_Donachy_Demo.pdf')
 @app.route('/super_secret/json_demo')
 def json_demo():
