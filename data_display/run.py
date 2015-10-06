40fdc79619c4e17d7b802889370824ce699d285b
SD <donachys@gmail.com>
2015-10-06 11:12:13 -0700

    updates for using supervisord/gunicorn

--- data_display/run.py
+++ data_display/run.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python
 from app import app
-app.run(host='0.0.0.0', port=80, debug = True, passthrough_errors=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug = True, passthrough_errors=True)
